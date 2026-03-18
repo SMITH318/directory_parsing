30a73a04d1050090b6ccb1f1826021a0f46b9d14
SMITH318 <SMITH318@GMAIL.COM>
2026-03-18 09:09:34 -0500

    Updates to utility/cleanup scripts. Consolidated removing batches and uploaded files cleanup scripts, and added cache cleanup.

--- scripts/00.00_clean_gemini.py
+++ scripts/00.00_clean_gemini.py
@@ -1,2 +1,64 @@
-import _remove_batches
-import _remove_uploaded_files
+from google import genai
+import os
+
+# --- Gemini API Configuration --- 
+API_KEY = os.getenv('GEMINI_API_KEY', 'YOUR_API_KEY')
+if API_KEY == 'YOUR_API_KEY' or not API_KEY:
+    print("ERROR: Gemini API key is not set.")
+    exit(1)
+
+client = None
+
+def remove_all_uploaded_files(close_created_client=True):
+    created_client = False
+    if not client:
+        created_client = True
+        client = genai.Client(api_key=API_KEY)
+
+    i = 0
+    # get all the files
+    for i, file in enumerate(client.files.list()):
+        # delete each file
+        client.files.delete(name=file.name)
+    print(f"deleted {i} files")
+
+    if close_created_client and created_client:
+        client.close()
+
+def remove_all_batches(close_created_client=True):
+    created_client = False
+    if not client:
+        created_client = True
+        client = genai.Client(api_key=API_KEY)
+
+    i = 0
+    # get all the jobs
+    for i, job in enumerate(client.batches.list()):
+        # delete each job
+        client.batches.delete(name=job.name)
+    print(f"deleted {i} batches")
+
+    if close_created_client and created_client:
+        client.close()
+
+def remove_all_caches(close_created_client=True):
+    created_client = False
+    if not client:
+        created_client = True
+        client = genai.Client(api_key=API_KEY)
+
+    i = 0
+    # get all the caches
+    for i, cache in enumerate(client.caches.list()):
+        # delete each cache
+        client.caches.delete(name=cache.name)
+    print(f"deleted {i} batches")
+
+    if close_created_client and created_client:
+        client.close()
+
+if __name__ == "__main__":
+    remove_all_uploaded_files(False)
+    remove_all_batches()
+    remove_all_caches()
+    client.close()
