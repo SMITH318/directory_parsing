5225fcf9f802d479e46c6462d5681f24f77f609d
SMITH318 <SMITH318@GMAIL.COM>
2026-03-18 09:23:50 -0500

    Updated cleaning script functions to take client as argument.

--- scripts/00.00_clean_gemini.py
+++ scripts/00.00_clean_gemini.py
@@ -1,20 +1,7 @@
 from google import genai
 import os
 
-# --- Gemini API Configuration --- 
-API_KEY = os.getenv('GEMINI_API_KEY', 'YOUR_API_KEY')
-if API_KEY == 'YOUR_API_KEY' or not API_KEY:
-    print("ERROR: Gemini API key is not set.")
-    exit(1)
-
-client = None
-
-def remove_all_uploaded_files(close_created_client=True):
-    created_client = False
-    if not client:
-        created_client = True
-        client = genai.Client(api_key=API_KEY)
-
+def remove_all_uploaded_files(client):
     i = 0
     # get all the files
     for i, file in enumerate(client.files.list()):
@@ -22,15 +9,7 @@ def remove_all_uploaded_files(close_created_client=True):
         client.files.delete(name=file.name)
     print(f"deleted {i} files")
 
-    if close_created_client and created_client:
-        client.close()
-
-def remove_all_batches(close_created_client=True):
-    created_client = False
-    if not client:
-        created_client = True
-        client = genai.Client(api_key=API_KEY)
-
+def remove_all_batches(client):
     i = 0
     # get all the jobs
     for i, job in enumerate(client.batches.list()):
@@ -38,15 +17,7 @@ def remove_all_batches(close_created_client=True):
         client.batches.delete(name=job.name)
     print(f"deleted {i} batches")
 
-    if close_created_client and created_client:
-        client.close()
-
-def remove_all_caches(close_created_client=True):
-    created_client = False
-    if not client:
-        created_client = True
-        client = genai.Client(api_key=API_KEY)
-
+def remove_all_caches(client):
     i = 0
     # get all the caches
     for i, cache in enumerate(client.caches.list()):
@@ -54,11 +25,16 @@ def remove_all_caches(close_created_client=True):
         client.caches.delete(name=cache.name)
     print(f"deleted {i} batches")
 
-    if close_created_client and created_client:
-        client.close()
 
 if __name__ == "__main__":
-    remove_all_uploaded_files(False)
-    remove_all_batches()
-    remove_all_caches()
+    # --- Gemini API Configuration --- 
+    API_KEY = os.getenv('GEMINI_API_KEY', 'YOUR_API_KEY')
+    if API_KEY == 'YOUR_API_KEY' or not API_KEY:
+        print("ERROR: Gemini API key is not set.")
+        exit(1)
+
+    client = genai.Client(api_key=API_KEY)
+    remove_all_uploaded_files(client)
+    remove_all_batches(client)
+    remove_all_caches(client)
     client.close()
