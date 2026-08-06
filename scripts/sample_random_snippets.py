7c8a0a9005dd96f829b06e5fdbeb094d1f1c91c9
SMITH318 <SMITH318@GMAIL.COM>
2026-08-06 17:36:05 -0500

    Random sampler updated to get at least X entries.

--- scripts/sample_random_snippets.py
+++ scripts/sample_random_snippets.py
@@ -7,7 +7,8 @@ import json
 import pandas as pd
 from pathlib import Path
 from collections import defaultdict
-
+import argparse
+import sys
 
 def load_metadata(metadata_path):
     """Load metadata from JSON file."""
@@ -15,9 +16,9 @@ def load_metadata(metadata_path):
         return json.load(f)
 
 
-def extract_all_snippets(metadata: list[dict]) -> pd.DataFrame:
+def get_all_snippets(metadata: list[dict]) -> pd.DataFrame:
     """
-    Extract all snippets from metadata, preserving context.
+    Get all snippets from metadata, preserving context.
     Returns a list of dicts with snippet info and context.
     """
     snippets = []
@@ -76,72 +77,86 @@ def extract_entries_for_snippet(
         (df['column'] == column)
     ]
 
-def extract_and_save_entries(snippets, csv_in_out:list[tuple[Path,Path]]):
+def extract_and_save_entries(snippets: pd.DataFrame, csv_in: Path, csv_out: Path) -> int:
     """Extract entries from CSVs for all snippets and save to combined CSV."""
     
-    for csv_in, csv_out in csv_in_out:
-        print(f"\nExtracting from {csv_in.name}")
-        df = pd.read_csv(csv_in)
-    
-        all_extracted = []
-        for snippet in snippets.itertuples():
-            filtered_df = extract_entries_for_snippet(df, snippet)
-            
-            if not filtered_df.empty:
-                print(
-                    f"  Found {len(filtered_df)} entries in for",
-                    f"{snippet.publication} page {snippet.page_number} column {snippet.column}"
-                )
-                all_extracted.append(filtered_df)
+    print(f"\nExtracting from {csv_in.name}")
+    df = pd.read_csv(csv_in)
+
+    all_extracted = []
+    for snippet in snippets.itertuples():
+        filtered_df = extract_entries_for_snippet(df, snippet)
         
-        sort_cols = ['publication', 'page_number', 'column']
-        df = pd.concat(all_extracted)
-        df = df.sort_values(by=sort_cols)
-                
-        # Save to CSV
-        df.to_csv(csv_out, index=False, mode='a')
-        print(f"  Total entries: {len(df)}, in {csv_out}")
-
-
-def main(num_samples=5):
+        if not filtered_df.empty:
+            print(
+                f"  Found {len(filtered_df)} entries in for",
+                f"{snippet.publication} page {snippet.page_number} column {snippet.column}"
+            )
+            all_extracted.append(filtered_df)
     
-    data_dir = Path(__file__).parent.parent / 'data'
-    metadata_path = data_dir / '01_preprocessed' / 'all_metadata.json'
-    csv_dir = Path(data_dir) / '04_extracted_entries_gemini_2026.03.18'
-    doc_csv = csv_dir / 'sampled_entries_docs_output.csv'
-    city_csv = csv_dir / 'sampled_entries_cities_output.csv'
+    if not all_extracted:
+        print(f"  No entries found for any of the sampled snippets")
+        return 0
 
+    # sort_cols = ['publication', 'page_number', 'column']
+    df = pd.concat(all_extracted)
+    # df = df.sort_values(by=sort_cols)
+            
+    # Save to CSV
+    df.to_csv(csv_out, index=False, mode='a', header=not csv_out.exists())
+    print(f"  Entries added: {len(df)}, in {csv_out}")
+    return len(df)
+
+
+def main(dataset: str, target_num_docs=500):
+    # Define the data directory
+    script_dir = Path(__file__).resolve().parent
+    project_root = script_dir.parent
+    data_dir = project_root / "data" / dataset
+
+    metadata_path = data_dir / '01_preprocessed' / 'all_metadata.json'
+    doc_csv = data_dir / 'sampled_entries_docs_output.csv'
+    city_csv = data_dir / 'sampled_entries_cities_output.csv'
+
+    num_samples = 5 # starting value
+    docs_so_far = 0
     # Load and process
     metadata = load_metadata(metadata_path)
     
     print(f"Extracting snippets...")
-    all_snippets = extract_all_snippets(metadata)
+    all_snippets = get_all_snippets(metadata)
     print(f"Total snippets found: {len(all_snippets)}")
-
-    # remove already sampled columns
-    if doc_csv.exists():
-        cols_to_match = ['publication', 'page_number', 'column']
-        doc_df = pd.read_csv(doc_csv)[cols_to_match].drop_duplicates()
-        merged = all_snippets.merge(doc_df, on=cols_to_match, how='left', indicator=True)
-        # Keep only rows from the 'left_only' source and drop the helper column
-        all_snippets = merged[merged['_merge'] == 'left_only'].drop(columns='_merge')
-        print(f"Snippets not previously selected: {len(all_snippets)}")
-
     
-    print(f"\nSampling {num_samples} random snippets...")
-    sampled = sample_snippets(all_snippets, num_samples)
+    while docs_so_far < target_num_docs:
+        # remove already sampled columns
+        remaining_snippets = all_snippets.copy()
+        if doc_csv.exists():
+            cols_to_match = ['publication', 'page_number', 'column']
+            doc_df = pd.read_csv(doc_csv)[cols_to_match].drop_duplicates()
+            merged = remaining_snippets.merge(doc_df, on=cols_to_match, how='left', indicator=True)
+            # Keep only rows from the 'left_only' source and drop the helper column
+            remaining_snippets = merged[merged['_merge'] == 'left_only'].drop(columns='_merge')
+            # print(f"Snippets not previously selected: {len(remaining_snippets)}")
+
+        
+        print(f"\nSampling {num_samples} random snippets...")
+        sampled = sample_snippets(remaining_snippets, num_samples)
+        
+        # Print results
+        print_snippets(sampled)
+
+        # If extraction mode, extract entries and save combined CSV
+        extract_and_save_entries(sampled, data_dir / "10_city_entries_sorted.csv", city_csv)
+        docs_so_far += extract_and_save_entries(sampled, data_dir / "10_doc_entries_sorted.csv", doc_csv)
+        if docs_so_far >= target_num_docs//2:
+            num_samples = 1
+        print(f"Total docs extracted so far: {docs_so_far}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Pick random columns from all_metadata.json and their extract entries from CSVs.")
+    parser.add_argument("dataset", help="Name of the dataset")
+    # parser.add_argument("--config", help="Path to JSON schema config file", required=True)
+    args = parser.parse_args()
     
-    # Print results
-    print_snippets(sampled)
-
-    # If extraction mode, extract entries and save combined CSV
-    extract_and_save_entries(
-        sampled, 
-        [
-            (csv_dir / "amd_1918_city_entries_sorted_deduped.csv", city_csv),
-            (csv_dir / "amd_1918_doc_entries_sorted_deduped.csv", doc_csv)
-        ]
-    )
-
-if __name__ == '__main__':
-    main()
+    exit_code = main(args.dataset)#, args.config)
+    sys.exit(exit_code)
